0f82571a3d98d31d0e349e374dec4aab9285dd13
s3rxio <cergotabagua222@gmail.com>
2023-10-20 00:15:07 +0300

    Init project

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="box">
+      <h1 className="box__heading">Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
